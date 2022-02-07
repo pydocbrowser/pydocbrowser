ab5c537c65351b7cf5fba262c4850ac8d37e2814
Martin Fischer <martin@push-f.com>
2022-02-07 16:23:29 +0100

    format deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,6 @@
-tar cf - -C dist . | ssh pydoc.dev 'sh -c "set -x; cd /var/www/pydoc.dev && rm -r * && pwd && tar xvf - && echo done"'
+tar cf - -C dist . | ssh pydoc.dev 'sh -c "
+set -x &&
+cd /var/www/pydoc.dev &&
+rm -r * &&
+tar xvf -
+"'
