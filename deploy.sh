316ad6f346774cde1816cdf14f1cd96bdf44b759
Martin Fischer <martin@push-f.com>
2022-02-07 16:25:44 +0100

    improve deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,10 @@
 tar cf - -C dist . | ssh pydoc.dev 'sh -c "
 set -x &&
 cd /var/www/pydoc.dev &&
-rm -r * &&
-tar xvf -
+([ ! -d tmp ] || rm -r tmp) &&
+mkdir tmp &&
+tar xvf - -C tmp &&
+([ ! -d dist ] || mv dist old) &&
+mv tmp dist &&
+rm -r old
 "'
