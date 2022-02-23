c834846cae6c2fb3065b277dbf577576aeeb3f4b
tristanlatr <tris.la.tr@gmail.com>
2022-02-23 16:23:16 -0500

    Fix iterativebuild.sh

--- iterativebuild.sh
+++ iterativebuild.sh
@@ -5,11 +5,10 @@
 
 set +e
 # For testing, it will create many jobs, a values more like 200 should be set for production
-python -m pydocbrowser --build-timeout 1
-set -e
-
+python -m pydocbrowser --build-timeout 0
 # Exit code 21 means it stopped generating docs because of the build timeout
 if [[ $? -eq 21 ]]; then
+    set -e
     echo 'Sending signal to build docs again'
     curl -XPOST -u "$GH_USERNAME:$GH_TOKEN" \
         -H "Accept: application/vnd.github.everest-preview+json" \
