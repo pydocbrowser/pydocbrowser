7d7fab7e4d40abd026de3d5770b8dfc3f9243b81
tristanlatr <tris.la.tr@gmail.com>
2022-07-13 14:17:29 -0400

    Fix the gh-latest-only.py script.
    Rebuilding everything now...

--- gh-latest-only.py
+++ gh-latest-only.py
@@ -1,10 +1,28 @@
 """
 We delete all the files in directories that are not the latest version 
-to try to reduce the size of the website because Github Pages only allows 1Gb.
+to reduce the size of the website because Github Pages only allows 1Gb.
 """
 from pathlib import Path
 import shutil
 import os
+from typing import Tuple
+
+def get_version(s:str) -> Tuple[int, int, int]:
+    parts = s.strip().split('.')
+    intparts = []
+    
+    for p in parts:
+        try:
+            v = int(p)
+        except:
+            v = 0
+        intparts.append(v)
+    
+    for _ in range(3-len(intparts)):
+        intparts.append(0)
+    
+    assert len(intparts)==3
+    return tuple(intparts)
 
 if __name__ == '__main__':
 
@@ -14,24 +32,36 @@ if __name__ == '__main__':
         # Not a docs folder
         if not entry.is_dir():
             continue
-        # A new latest symlink, we leave it untouched
-        # because this script relies on the fact that github
-        # turn symlinks into actual folders, we must run it only
-        # on older generated docs
+        
+        # Determine the folders to delete...
+
+        names = [_e.name for _e in entry.iterdir() if _e.name!='latest']
+        versions = {get_version(_v):_v for _v in names}
+        latest_version = max(versions.keys())
+        latest_version_name = versions[latest_version]
+
+        names_to_delete = [_n for _n in names if _n != latest_version_name]
+
+        # Sanity checks...
         latest = (entry / 'latest')
         assert latest.exists()
-
-        if latest.is_symlink():
-            continue
-
-        assert latest.is_dir()
+        
         # Deletes everything except latest directory
-        for subfolder in entry.iterdir():
+        for subfolder_name in names_to_delete:
+            subfolder = entry / subfolder_name
             if not subfolder.is_dir():
+                print(f'not a directory: {subfolder}')
                 continue
             if subfolder.name == 'latest':
+                print(f'latest directory: {subfolder}')
                 continue
             remove = subfolder.as_posix()
+            
+            # skips already removed entries
+            if len(list(subfolder.iterdir()))==1 and \
+               (subfolder/'objects.inv').exists():
+                continue
+            
             print(f'Removing files in {remove!r} (except objects.inv)')
             for _f in subfolder.iterdir():
                 if _f.is_dir():
